d98daef7236c445ba9837f3cfccb9e8209d15c59
antonmaenpaa <antonmaenpaa@gmail.com>
2023-01-09 17:01:20 +0100

    projects update

--- src/projectsList.js
+++ src/projectsList.js
@@ -9,12 +9,4 @@ export const projects = [
         href: "https://triabygg.fi",
         img: triabygg,
     },
-    {
-        title: 'FRIWWIRETOUCH Website',
-        description: 'This is a website portfolio. The client wanted a portfolio where she could show her work. Created with ReactJS and Strapi CMS.',
-        id: '2',
-        href: "https://www.friwwiretouch.com/",
-        img: friwwiretouch,
-    },
-
 ]
